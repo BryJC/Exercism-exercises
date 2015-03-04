33c0e10599ca7b48f11d43d3abfbacb3085b9f4e
BryJC <bryjc.projects@gmail.com>
2015-03-04 15:21:18 -0800

    second attempt

--- python/nth-prime/prime.py
+++ python/nth-prime/prime.py
@@ -1,12 +1,12 @@
 def nth_prime(position):
     
     primes_list = [2, 3, 5, 7]
-    count = 10
+    c = 10
     while len(primes_list) <= position:
-        if any(x == 0 for x in [count % i for i in range(2,9)]):
-            count += 1
-        elif all(x != 0 for x in [count % i for i in range(2,9)]):
-            primes_list.append(count)
-            count += 1
+        if any(x == 0 for x in [c % i for i in range(2, 1+int(c**0.5))]):
+            c += 1
+        elif all(x != 0 for x in [c % i for i in range(2, 1+int(c**0.5))]):
+            primes_list.append(c)
+            c += 1
     return primes_list[position-1]
     
