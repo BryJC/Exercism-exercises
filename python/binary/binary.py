e78ceaecf1cc5513443781b9ed9f63651831ac36
BryJC <bryjc.projects@gmail.com>
2015-03-14 01:21:45 -0700

    moar submissions

--- python/binary/binary.py
+++ python/binary/binary.py
@@ -1,6 +1,12 @@
-def parse_binary(n):
+def parse_binary(num):
     try:
-        return int(n, 2)
+        int(num, 2)
     except:
         raise ValueError
-#placeholder solution
+    lst = []
+    n = 1
+    for i in num[::-1]:
+        if i == '1':
+            lst.append(2**(n-1))
+        n += 1
+    return sum(lst)
