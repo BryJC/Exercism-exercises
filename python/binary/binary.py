be85dc592e73a11f7a3d20906c3d27d6d1569ab4
BryJC <bryjc.projects@gmail.com>
2015-03-19 22:39:18 -0700

    updated binary solution

--- python/binary/binary.py
+++ python/binary/binary.py
@@ -1,12 +1,8 @@
 def parse_binary(num):
-    try:
-        int(num, 2)
-    except:
-        raise ValueError
-    lst = []
-    n = 1
-    for i in num[::-1]:
-        if i == '1':
-            lst.append(2**(n-1))
-        n += 1
-    return sum(lst)
+    total = 0
+    for i, bit in enumerate(num[::-1]):
+        if (bit != '1') and (bit != '0'):
+            raise ValueError
+        elif bit == '1':
+            total += 2**(i)
+    return total
