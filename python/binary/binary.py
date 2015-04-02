c13562bc0aeca30f939ae87c123aa99fffe05d05
BryJC <bryjc.projects@gmail.com>
2015-04-01 17:25:54 -0700

    adding files

--- python/binary/binary.py
+++ python/binary/binary.py
@@ -1,6 +1,8 @@
-def parse_binary(n):
-    try:
-        return int(n, 2)
-    except:
-        raise ValueError
-#placeholder solution
+def parse_binary(num):
+    total = 0
+    for i, bit in enumerate(num[::-1]):
+        if (bit != '1') and (bit != '0'):
+            raise ValueError
+        elif bit == '1':
+            total += 2**(i)
+    return total
